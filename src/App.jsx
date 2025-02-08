bf1f6279921b8e1a013e327b8f54d2b092b5a71b
rexzea <satriabam01@gmail.com>
2025-02-08 11:24:45 +0700

    Update App.jsx (Obama)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-import { Button } from "./components/button";
 import axios from "axios";
 
 function App() {
@@ -29,38 +28,42 @@ function App() {
   }, []);
 
   return (
-    <div className="flex justify-center items-center ">
-      <div className="flex gap-4 items-center">
-        {error ? (
-          <h1>ada error nih</h1>
-        ) : loading ? (
-          <div className="h-screen flex justify-center items-center">
-            <img
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png`}
-            />
+    <div className="bg-sky-500 p-8 rounded-lg shadow-lg">
+      {error ? (
+        <h1 className="text-red-500 text-center">ada error nihh</h1>
+      ) : loading ? (
+        <div className="bg-yellow-500 flex justify-center items-center p-8 rounded-lg">
+          <div className="spinner-border text-primary" role="status">
+            <span className="sr-only">Loading...</span>
           </div>
-        ) : (
-          <div className="">
-            <h1>Pokemon Data</h1>
-            <div className="flex flex-wrap justify-center gap-8">
-              {pokemonData.map((halo, index) => (
-                <div
-                  key={index}
-                  className="border-2 flex flex-col items-center"
+        </div>
+      ) : (
+        <div className="bg-white p-8 rounded-lg shadow-md">
+          <h1 className="text-lg font-bold text-center">Pokemon Data</h1>
+          <div className="flex flex-wrap justify-center gap-5 p-4">
+            {pokemonData.map((nama, index) => (
+              <div
+                key={index}
+                className="border-2 flex flex-col items-center bg-blue-200 p-8 rounded-lg shadow-md hover:shadow-lg transition duration-300"
+              >
+                <h1 className="text-lg font-bold">{index + 1}</h1>
+                <img
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                    index + 1
+                  }.png`}
+                  className="w-20 h-20"
+                />
+                <h1 className="text-lg font-bold">{nama.name}</h1>
+                <button
+                  class="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 >
-                  <h1>{index + 1}</h1>
-                  <img
-                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-                      index + 1
-                    }.png`}
-                  />
-                  <h1>{halo.name}</h1>
-                </div>
-              ))}
-            </div>
+                  Link : {nama.url}
+                </button>
+              </div>
+            ))}
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
